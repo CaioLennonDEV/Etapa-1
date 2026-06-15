67e7865ae9a77384f5edd437960caf0f2791e6c5
Caio Lennon <caiolennon09@gmail.com>
2026-06-15 12:20:51 -0300

    Implementa rota de teste no API e atualiza script de inicialização para modo de observação

--- api.js
+++ api.js
@@ -3,6 +3,16 @@ import express from 'express';
 // Chamada do express
 const app = express();
 
+// Objeto de teste
+const objeto = [{nome: 'João', idade: 20}, 
+    {nome: 'Maria', idade: 21}];
+
+// Rota de teste
+app.get('/', (req, res) => {
+    res.json(objeto);
+});
+// req e res são objetos que representam a requisição e a resposta HTTP
+
 // Porta de conexão
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
